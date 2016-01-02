54cdf86fb823917149645e9250c5eef6b2a496ee
Rob Bert <robertinho88.rb@gmail.com>
2016-01-02 12:43:21 +0100

    Add initial files and config

---
+++ .chef/knife.rb
@@ -0,0 +1,11 @@
+# See http://docs.chef.io/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "robart-bb-88-node"
+client_key               "#{current_dir}/robart-bb-88.pem"
+validation_client_name   "devpass-validator"
+validation_key           "#{current_dir}/devpass-validator.pem"
+chef_server_url          "https://api.chef.io/organizations/devpass"
+cookbook_path            ["#{current_dir}/../cookbooks"]
