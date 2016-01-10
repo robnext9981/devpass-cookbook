95cb470c52e4e94dd1b7ab577d14155db9e778b5
Rob Bert <robertinho88.rb@gmail.com>
2016-01-10 22:38:12 +0100

    add default cookbook copyright data

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -3,9 +3,12 @@
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "robart-bb-88-node"
+node_name                "robart-bb-88"
 client_key               "#{current_dir}/robart-bb-88.pem"
 validation_client_name   "devpass-validator"
 validation_key           "#{current_dir}/devpass-validator.pem"
 chef_server_url          "https://api.chef.io/organizations/devpass"
 cookbook_path            ["#{current_dir}/../cookbooks"]
+cookbook_copyright       "DevPass Co."
+cookbook_license         "apachev2"
+cookbook_email           "myemail@devpass.com"
