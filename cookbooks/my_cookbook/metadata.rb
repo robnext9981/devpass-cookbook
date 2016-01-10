132380b4461c049af925680d0dceced9239df21d
Rob Bert <robertinho88.rb@gmail.com>
2016-01-10 22:39:13 +0100

    Add my_cookbook

---
+++ cookbooks/my_cookbook/metadata.rb
@@ -0,0 +1,7 @@
+name             'my_cookbook'
+maintainer       'DevPass Co.'
+maintainer_email 'myemail@devpass.com'
+license          'Apache 2.0'
+description      'Installs/Configures my_cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
